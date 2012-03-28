7e3e27d03ae2576d868a6658da4a2c24c57a61f0
sue445 <sue445@sue445.net>
2012-03-29 00:32:09 +0900

    impl UserLogin
    add skelton
    
    add red test
    
    impl apply (but red test)
    
    WIP
    
    use tester
    
    remove tester
    
    impl UserLogin

---
+++ src/main/java/net/sue445/s3tiger/rules/UserLogin.java
@@ -0,0 +1,59 @@
+package net.sue445.s3tiger.rules;
+
+import org.junit.rules.ExternalResource;
+import org.junit.rules.RuleChain;
+import org.slim3.tester.TestEnvironment;
+
+import com.google.apphosting.api.ApiProxy;
+
+/**
+ * user login test.
+ * this rule required {@link RuleChain}.
+ * please use {@link AppEngineResource} or {@link ControillerResource}.
+ * <pre>
+ * public class SomeServiceTest{
+ *     &#064;Rule
+ *     public RuleChain ruleChain = RuleChain.outerRule(new AppEngineResource()).around(new UserLogin(TEST_EMAIL_ADDRESS));
+ *
+ *     &#064;Test
+ *     public void test(){
+ *     }
+ * }
+ * </pre>
+ * @author sue445
+ * @since 0.0.3
+ */
+public class UserLogin extends ExternalResource {
+	private final String email;
+	private final boolean admin;
+
+
+	/**
+	 * login with anonimous user (not admin)
+	 * @param email		login mail address
+	 */
+	public UserLogin(String email){
+		this(email, false);
+	}
+
+	/**
+	 * login with anonimous user
+	 * @param email		login mail address
+	 * @param admin		whether admin user
+	 */
+	public UserLogin(String email, boolean admin){
+		this.email = email;
+		this.admin = admin;
+	}
+
+	@Override
+	protected void before() throws Throwable {
+		TestEnvironment environment = (TestEnvironment) ApiProxy.getCurrentEnvironment();
+		if(environment == null){
+			throw new NullPointerException("Not initialized TestEnvironment. please use RuleChain");
+		}
+		environment.setEmail(email);
+		environment.setAdmin(admin);
+	}
+
+}
