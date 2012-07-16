fdb36c063a5afe3309c5015ea6dbea1ba636fa20
sue445 <sue445@sue445.net>
2012-07-16 19:27:14 +0900

    ver 0.0.4

--- src/test/java/net/sue445/s3tiger/rules/UserLoginTest.java
+++ src/test/java/net/sue445/s3tiger/rules/UserLoginTest.java
@@ -2,12 +2,14 @@ package net.sue445.s3tiger.rules;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
+import static org.junit.Assume.*;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.rules.RuleChain;
 import org.junit.runner.RunWith;
+import org.slim3.util.AppEngineUtil;
 
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
@@ -16,13 +18,14 @@ import com.google.appengine.api.users.UserServiceFactory;
 public class UserLoginTest{
 	private static final String TEST_EMAIL_ADDRESS = "hoge@foo.com";
 
-
 	public static class WhenUserLogin{
 		@Rule
 		public RuleChain ruleChain = RuleChain.outerRule(new AppEngineResource()).around(new UserLogin(TEST_EMAIL_ADDRESS));
 
 		@Test
 		public void isLogin() {
+			assumeThat(AppEngineUtil.isProduction(), is(false));
+
 			UserService userService = UserServiceFactory.getUserService();
 			assertThat(userService.isUserLoggedIn(), is(true));
 			assertThat(userService.getCurrentUser().getEmail(), is(TEST_EMAIL_ADDRESS));
@@ -36,10 +39,23 @@ public class UserLoginTest{
 
 		@Test
 		public void isLogin() {
+			assumeThat(AppEngineUtil.isProduction(), is(false));
+
 			UserService userService = UserServiceFactory.getUserService();
 			assertThat(userService.isUserLoggedIn(), is(true));
 			assertThat(userService.getCurrentUser().getEmail(), is(TEST_EMAIL_ADDRESS));
 			assertThat(userService.isUserAdmin(), is(true));
+		}
+	}
+
+	public static class WhenNotLogined{
+		@Rule
+		public AppEngineResource resource = new AppEngineResource();
+
+		@Test
+		public void isLogin() {
+			UserService userService = UserServiceFactory.getUserService();
+			assertThat(userService.isUserLoggedIn(), is(false));
 		}
 	}
 
