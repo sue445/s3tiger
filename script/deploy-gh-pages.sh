db53f4ff05a2f6bcac40514295ac9efe0f757806
sue445 <sue445@sue445.net>
2013-01-18 01:46:39 +0900

    add jenkins script
    rename

---
+++ script/deploy-gh-pages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+readonly WORKSPACE=$1
+readonly BUILD_NUMBER=$2
+readonly BUILD_ID=$3
+
+readonly TMP_DIR=tmp
+readonly REPO_URL="git@github.com:sue445/s3tiger.git"
+readonly BRANCH="gh-pages"
+
+rm -rf ${WORKSPACE}/${TMP_DIR}
+
+git clone ${REPO_URL} --branch ${BRANCH} ${TMP_DIR}
+
+cd ${TMP_DIR}
+
+cp -r ../site/* .
+
+git add .
+git commit -am "build by jenkins : ${BUILD_NUMBER} ${BUILD_ID}"
+
+git push origin ${BRANCH}
+
