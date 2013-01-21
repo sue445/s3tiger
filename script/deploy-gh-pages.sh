da8cd5213b25747e2e80d4289e4465c84584cb5e
sue445 <sue445@sue445.net>
2013-01-22 00:27:35 +0900

    refactor jenkins script

--- script/deploy-gh-pages.sh
+++ script/deploy-gh-pages.sh
@@ -5,17 +5,24 @@ readonly BUILD_NUMBER=$2
 readonly BUILD_ID=$3
 
 readonly TMP_DIR=tmp
+readonly REPO_DIR=repo
+readonly SITE_DIR=site
 readonly REPO_URL="git@github.com:sue445/s3tiger.git"
 readonly BRANCH="gh-pages"
 
-rm -rf ${WORKSPACE}/${TMP_DIR}
+mkdir -p "${WORKSPACE}/${TMP_DIR}"
+cd       "${WORKSPACE}/${TMP_DIR}"
 
-git clone ${REPO_URL} --branch ${BRANCH} ${TMP_DIR}
+if [ -e "${WORKSPACE}/${TMP_DIR}/${REPO_DIR}" ]; then
+  cd "${WORKSPACE}/${TMP_DIR}/${REPO_DIR}"
+  git checkout ${BRANCH}
+else
+  git clone ${REPO_URL} --branch ${BRANCH} ${REPO_DIR}
+  cd "${WORKSPACE}/${TMP_DIR}/${REPO_DIR}"
+fi
 
-cd ${TMP_DIR}
-
-rm -rf ./site/*
-cp -r ../site .
+rm -rf "${WORKSPACE}/${TMP_DIR}/${REPO_DIR}/${SITE_DIR}/*"
+cp -r  "${WORKSPACE}/${SITE_DIR}" .
 
 git add .
 git commit -am "build by jenkins : ${BUILD_NUMBER} ${BUILD_ID}"
